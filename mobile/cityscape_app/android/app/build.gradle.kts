0ece38e64b77f56ad1c1713cb5cb47ceaa48e4f2
Damien <damien.gilbon@gmail.com>
2026-08-02 17:14:50 +0200

    chore: bump version 0.3.17+31 -> 0.3.18+32
    Prepare release AAB pour le lot UX/createur (badge age, credit createur,
    historique visible, rejouer).

--- mobile/cityscape_app/android/app/build.gradle.kts
+++ mobile/cityscape_app/android/app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.roadpapattes.cityscape"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 31
-        versionName = "0.3.17"
+        versionCode = 32
+        versionName = "0.3.18"
 
         // Injecte le placeholder utilisé par AndroidManifest.xml
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
