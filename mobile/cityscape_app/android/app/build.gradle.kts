6528c32958c3debee09531d64ca5f0c2d1ebfc5c
Damien <damien.gilbon@gmail.com>
2026-09-09 09:38:13 +0200

    chore: bump version 0.3.26+40 -> 0.3.27+41
    min_version backend volontairement pas encore remonté - attendre que
    la version soit réellement disponible au téléchargement avant
    d'activer la mise à jour forcée (voir mémoire release_min_version).

--- mobile/cityscape_app/android/app/build.gradle.kts
+++ mobile/cityscape_app/android/app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.roadpapattes.cityscape"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 40
-        versionName = "0.3.26"
+        versionCode = 41
+        versionName = "0.3.27"
 
         // Injecte le placeholder utilisé par AndroidManifest.xml
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
